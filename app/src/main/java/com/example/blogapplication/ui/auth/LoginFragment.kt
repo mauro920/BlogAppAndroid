04e03ae269a6674b9ec36792a6ddc354973a4d55
Mauro Peña <mauroalejandropena@gmail.com>
2022-04-07 20:12:13 -0300

    functions for the login logic

--- app/src/main/java/com/example/blogapplication/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/blogapplication/ui/auth/LoginFragment.kt
@@ -5,15 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.blogapplication.R
 import com.example.blogapplication.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private lateinit var binding: FragmentLoginBinding
+    private val firebaseAuth by lazy { FirebaseAuth.getInstance() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentLoginBinding.bind(view)
+        isUserLogged()
+        doLogin()
+    }
+
+
+    private fun isUserLogged() {
+        firebaseAuth.currentUser?.let {
+            findNavController().navigate(R.id.action_loginFragment_to_homeScreenFragment)
+        }
+    }
+
+    private fun doLogin(){
+        binding.loginBtn.setOnClickListener {
+            val email = binding.emailInput.editText.toString().trim()
+            val password = binding.passwordInput.editText.toString().trim()
+            validateCredentials(email,password)
+            login(email,password)
+        }
+    }
+
+    private fun validateCredentials(email: String, password: String){
+        if (email.isEmpty()){
+            binding.emailInput.error = "E-Mail is empty"
+        }
+        if (password.isEmpty()){
+            binding.passwordInput.error = "Password is empty"
+        }
+    }
+    private fun login(email:String, password: String){
+
     }
 }
