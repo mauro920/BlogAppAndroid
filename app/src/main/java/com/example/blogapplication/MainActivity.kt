7e2d366c5dc682cd48f7813865d3d4ac80b7d4ff
Mauro Peña <mauroalejandropena@gmail.com>
2022-04-08 20:50:32 -0300

    navigation bar fixed

--- app/src/main/java/com/example/blogapplication/MainActivity.kt
+++ app/src/main/java/com/example/blogapplication/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.blogapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.example.blogapplication.core.hide
+import com.example.blogapplication.core.show
 import com.example.blogapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,5 +23,26 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
+
+        observeDestinationChange(navController)
+    }
+
+    private fun observeDestinationChange(navController: NavController) {
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+
+                R.id.loginFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+
+                R.id.registerFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+
+                else -> {
+                    binding.bottomNavigationView.show()
+                }
+            }
+        }
     }
 }
