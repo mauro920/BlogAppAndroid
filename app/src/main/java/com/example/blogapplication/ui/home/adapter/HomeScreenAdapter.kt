36f3ce76389d600df38a8a48fda2f5805e263f6a
Mauro Peña <mauroalejandropena@gmail.com>
2022-04-16 15:26:11 -0300

    added time stamp in posts.

--- app/src/main/java/com/example/blogapplication/ui/home/adapter/HomeScreenAdapter.kt
+++ app/src/main/java/com/example/blogapplication/ui/home/adapter/HomeScreenAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.Adapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.blogapplication.core.BaseViewHolder
+import com.example.blogapplication.core.TimeUtils
 import com.example.blogapplication.data.model.Post
 import com.example.blogapplication.databinding.PostItemViewBinding
 
@@ -35,12 +36,17 @@ class HomeScreenAdapter(private val postList: List<Post>) :
             Glide.with(context).load(item.post_image).centerCrop().into(binding.postImage)
             Glide.with(context).load(item.profile_picture).centerCrop().into(binding.profilePicture)
             binding.profileName.text = item.profile_name
-            binding.postTimestamp.text = "Hace 2 Horas"
             if (item.post_description.isEmpty()){
                 binding.postDescription.visibility = View.GONE
             } else {
                 binding.postDescription.text = item.post_description
             }
+
+
+            val createdAt = item.created_at?.time?.div(1000L)?.let {
+                TimeUtils.getTimeAgo(it.toInt())
+            }
+            binding.postTimestamp.text = createdAt
         }
 
     }
