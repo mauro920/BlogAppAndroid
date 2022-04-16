b60ea1ed2ba6ba379133e6b841fde4081f834f24
Mauro Peña <mauroalejandropena@gmail.com>
2022-04-16 15:37:43 -0300

    correction of the order of the post list

--- app/src/main/java/com/example/blogapplication/data/remote/home/HomeScreenDataSource.kt
+++ app/src/main/java/com/example/blogapplication/data/remote/home/HomeScreenDataSource.kt
@@ -4,13 +4,14 @@ import com.example.blogapplication.core.Resource
 import com.example.blogapplication.data.model.Post
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.coroutines.tasks.await
 
 class HomeScreenDataSource {
 
     suspend fun getLatestPosts(): Resource<List<Post>> {
         val postList = mutableListOf<Post>()
-        val querySnapshot = FirebaseFirestore.getInstance().collection("posts").get().await()
+        val querySnapshot = FirebaseFirestore.getInstance().collection("posts").orderBy("created_at", Query.Direction.DESCENDING).get().await()
         for (post in querySnapshot.documents) {
             post.toObject(Post::class.java)?.let { fbPost ->
                 fbPost.apply { created_at = post.getTimestamp("created_at",DocumentSnapshot.ServerTimestampBehavior.ESTIMATE)?.toDate() }
